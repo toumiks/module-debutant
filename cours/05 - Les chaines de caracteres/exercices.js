3b958b78f9bed13f2184282e8b93d7bb5e68184b
toumiks <53621643+toumiks@users.noreply.github.com>
2019-08-02 23:47:44 +0200

    Update exercices.js

--- cours/05 - Les chaines de caracteres/exercices.js
+++ cours/05 - Les chaines de caracteres/exercices.js
@@ -9,20 +9,24 @@
  */
 
 // Exercice 0
-
+console.log('Javascript est facile');
 // Exercice 1
-
+console.log("Javascript c'est facile");
 // Exercice 2
-
+console.log('C/n'est Lao Tseu qui a dit : "L'échec est le fondement de la réussite"');
 // Exercice 3
-
+  console.log("Afficher un \\ n'est pas si évident");
 // Exercice 4
-
+console.log("Ceci est la première ligne /n Ceci est la deuxième ligne");
 // Exercice 5
-
+console.log(` Un jour le JavaScript n'aura plus de secrets pour moi
+              Lire du code ne sera plus une gageure mais une joie
+              Mes doigts seront habitués à tous ces symboles particuliers
+              Et ma créativité de développeur pourra enfin s'exprimer
+`)
 // Chassez le bogue 0
 // Retirez les commentaires devant le code de Tom pour commencer
-// console.log("Bonjour' + 'Bob' + ', aujourd'hui nous faisons une promo 'la fidélité paye' !");
+ console.log("Bonjour' + 'Bob' + ', aujourd'hui nous faisons une promo 'la fidélité paye' !");
 
 // Chassez le bogue 1
 // Retirez les commentaires devant le code de Tom pour commencer (les 3 lignes ci-dessous)
